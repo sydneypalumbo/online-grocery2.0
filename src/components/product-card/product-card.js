176912bdfbe89f0feca8be28c277ee75e0f415c6
SPalumbo <sydneypalumbo@bitbucket.org>
2017-11-17 09:31:10 -0500

    basic product grid

--- src/components/product-card/product-card.js
+++ src/components/product-card/product-card.js
@@ -1,3 +1,18 @@
 import React from 'react'
 import './product-card.scss'
 
+export default class ProductCard extends React.Component {
+    render() {
+        return (
+            <div className='product-card'>
+                <img className='product-card-image' src={this.props.src}/>
+                <div className='product-card-name'>{this.props.name}</div>
+                <div className='product-card-size'>{this.props.size}</div>
+                <div className='product-card-buttons'>
+                    <div className='product-card-price'>${this.props.price}</div>
+                    <img className='product-card-add-to-cart' src={'../../../src/images/trolley-clipart.png'}/>
+                </div>
+            </div>
+        )
+    }
+}
--- src/components/product-grid/product-grid.js
+++ src/components/product-grid/product-grid.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import './product-grid.scss'
+import ProductCard from '../product-card/product-card'
+
+const products = [{
+        name: "food 1",
+        src: "http://www.publix.com/images/products/5000/005378-600x600-A.jpg",
+        price: 10,
+        size: "5 oz"
+    },
+    {
+        name: "food 2",
+        src: "http://www.publix.com/images/products/5000/005378-600x600-A.jpg",
+        price: 5,
+        size: "5 oz"
+    },
+    {
+        name: "food 1",
+        src: "http://www.publix.com/images/products/5000/005378-600x600-A.jpg",
+        price: 10,
+        size: "5 oz"
+    },
+    {
+        name: "food 2",
+        src: "http://www.publix.com/images/products/5000/005378-600x600-A.jpg",
+        price: 5,
+        size: "5 oz"
+    }]
 
 export default class ProductGrid extends React.Component {
 
-
     render() {
+        let productCards = products.map(product => {
+            return (
+                <div className='product-grid-item'>
+                    <ProductCard {...product}/>
+                </div>
+            )
+        })
         return (
-            <div>hey</div>
+            <div>{productCards}</div>
         )
     }
 
