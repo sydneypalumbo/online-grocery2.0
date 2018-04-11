2b6d910f9d01ed3c52d2b538ad9ee4f0ad3b6afa
Lauren Katz <lbk12@duke.edu>
2018-04-11 17:36:40 -0400

    fixed cart null issue

--- src/components/product-card/product-card.js
+++ src/components/product-card/product-card.js
@@ -19,7 +19,7 @@ export default class ProductCard extends React.Component {
     }
 
     handleAddToCart() {
-        this.props.handleAddToCart(this.props, this.state.quantity)
+        this.props.handleAddToCart(this.props.product, this.state.quantity)
         axios.post('/user', {
           sessionID:this.props.sessionID,
           actionType: "add",
